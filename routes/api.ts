6f4a7e27a545e33584e80053dbe7f4ae106d5a67
makrem41298 <makrem050@live.fr>
2026-04-02 10:28:07 +0100

    add made trafiGrif controller

--- routes/api.ts
+++ routes/api.ts
@@ -2,6 +2,12 @@ import { Application } from "express";
 import {getProfile, login, logout, refreshToken, register} from "../controllers/auth.controller";
 import test from "node:test";
 import {authMiddleware} from "../middlewares/auth.middleware";
+import {
+    createTarifGrid, deleteTarifGrid,
+    getAllTarifGrids,
+    getTarifGridById,
+    updateTarifGrid
+} from "../controllers/tarifGrid.controller";
 
 
 export default function routes(app: Application): void {
@@ -11,10 +17,20 @@ export default function routes(app: Application): void {
             message: "Welcome back!",
         })
     });
+
+    //auth users
     app.post("/register", register);
     app.post("/login", login);
     app.post("/refresh", refreshToken);
 // 🔐 Protected routes
     app.post("/logout", authMiddleware, logout);
     app.get("/profile", authMiddleware, getProfile);
+
+
+//tariff grids
+    app.post("/tarif-grid", createTarifGrid);
+    app.get("/tarif-grid", getAllTarifGrids);
+    app.get("/tarif-grid/:id", getTarifGridById);
+    app.put("/tarif-grid/:id", updateTarifGrid);
+    app.delete("/tarif-grid/:id", deleteTarifGrid);
 }
